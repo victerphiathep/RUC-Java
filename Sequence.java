b0fed7b5d3dd2338850982d0ae72ce7c28a06405
Victer <victerphiathep@gmail.com>
2024-01-31 21:11:38 -0500

    Added class

--- Sequence.java
+++ Sequence.java
@@ -15,19 +15,57 @@ public class Sequence {
         return values.toString();
     }
 
+    // E7.20: Append method
     public Sequence append(Sequence other) {
         Sequence result = new Sequence();
-
-        // Add all elements of this sequence to the new sequence
         for (int value : this.values) {
             result.add(value);
         }
-
-        // Add all elements of the other sequence to the new sequence
         for (int value : other.values) {
             result.add(value);
         }
-
         return result;
+    }
+
+    // E7.21: Merge method
+    public Sequence merge(Sequence other) {
+        Sequence result = new Sequence();
+        int i = 0, j = 0;
+        while (i < this.values.size() && j < other.values.size()) {
+            result.add(this.values.get(i++));
+            result.add(other.values.get(j++));
+        }
+        // Add remaining elements
+        while (i < this.values.size()) {
+            result.add(this.values.get(i++));
+        }
+        while (j < other.values.size()) {
+            result.add(other.values.get(j++));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Test the Sequence class
+        Sequence a = new Sequence();
+        a.add(1);
+        a.add(4);
+        a.add(9);
+        a.add(16);
+
+        Sequence b = new Sequence();
+        b.add(9);
+        b.add(7);
+        b.add(4);
+        b.add(9);
+        b.add(11);
+
+        Sequence appended = a.append(b);
+        Sequence merged = a.merge(b);
+
+        System.out.println("Sequence a: " + a);
+        System.out.println("Sequence b: " + b);
+        System.out.println("Appended sequence: " + appended);
+        System.out.println("Merged sequence: " + merged);
     }
 }
